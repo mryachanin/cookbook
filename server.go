e77b94ce846e9c09d0c76c13c960ca5f7ef6b659
Mike Yachanin <michael@yachanin.com>
2018-12-02 02:53:10 -0800

    Return rendered yaml in http response
    Refs:
     - http://www.will3942.com/creating-blog-go
     - https://golang.org/pkg/html/template/
     - https://godoc.org/gopkg.in/yaml.v2#Unmarshal

--- server.go
+++ server.go
@@ -1,16 +1,67 @@
 package main
 
 import (
+  "gopkg.in/yaml.v2"
 	"html/template"
+  "io/ioutil"
 	"log"
 	"net/http"
 	"path"
+  "path/filepath"
 )
 
+type Ingredient struct {
+  Quantity string
+  Name string
+}
+
+type Source struct {
+  Name string
+  Link string
+}
+
+type Recipe struct {
+  Name string
+  Ingredients []Ingredient
+  Instructions []string
+  Yields string
+  Keeps_for string
+  Prep_time string
+  Total_time string
+  Notes []string
+  Tags []string
+  Source Source
+}
+
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-	data := "Hello World!"
+	recipes, err := getRecipes()
+  if (err != nil) {
+    http.Error(w, err.Error(), 500)
+    return
+  }
 	t := template.Must(template.ParseFiles(path.Join("templates", "index.html")))
-	t.Execute(w, data)
+	t.Execute(w, recipes)
+}
+
+func getRecipes() ([]Recipe, error) {
+  recipes := []Recipe{}
+  paths, err := filepath.Glob("public/recipes/*")
+  if (err != nil) {
+    return nil, err
+  }
+  for _, path := range paths {
+    b, err := ioutil.ReadFile(path)
+  	if err != nil {
+      return nil, err
+  	}
+
+    var r Recipe
+    if err = yaml.Unmarshal(b, &r); err != nil {
+  		return nil, err
+  	}
+    recipes = append(recipes, r)
+  }
+  return recipes, nil
 }
 
 func main() {
