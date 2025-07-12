2361e7d6ba87e290d102cb7ddd18dbce97fae6d3
Mike Yachanin <michael@yachanin.com>
2025-07-12 02:13:22 -0400

    feat: Update html/css vibe

--- web/app/handler.go
+++ web/app/handler.go
@@ -13,9 +13,15 @@ import (
 func HandleRequests(c *config.Config, db *couchdb.Database) {
   router := httprouter.New()
 
+  // Static CSS file serving
+  router.GET("/styles.css", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    w.Header().Set("Content-Type", "text/css")
+    http.ServeFile(w, r, "web/template/styles.css")
+  })
+
   router.GET("/", wrap(view.GetIndex, db))
-  router.GET("/recipe", wrap(view.CreateRecipe, db))
-  router.POST("/recipe", wrap(view.PostRecipe, db))
+  router.GET("/create", wrap(view.CreateRecipe, db))
+  router.POST("/create", wrap(view.PostRecipe, db))
   router.GET("/recipe/:id", wrap(view.GetRecipe, db))
 
   url := c.Host + ":" + strconv.Itoa(c.Port)
