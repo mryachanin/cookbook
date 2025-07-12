d9ecc9b813ef8634735289b9298031721e93cd76
Mike Yachanin <michael@yachanin.com>
2025-07-12 04:13:05 -0400

    fix: Recipe saving was broken after ui changes

--- web/app/view/recipe.go
+++ web/app/view/recipe.go
@@ -5,10 +5,9 @@ import (
   appTemplate "github.com/mryachanin/cookbook/web/template"
   "github.com/julienschmidt/httprouter"
   "github.com/rhinoman/couchdb-go"
+  "github.com/google/uuid"
   "errors"
-  "fmt"
   "html/template"
-  "io/ioutil"
   "net/http"
 )
 
@@ -38,7 +37,88 @@ func CreateRecipe(w http.ResponseWriter, r *http.Request, ps httprouter.Params, 
 }
 
 func PostRecipe(w http.ResponseWriter, r *http.Request, ps httprouter.Params, db *couchdb.Database) {
-  bodyBytes, _ := ioutil.ReadAll(r.Body)
-  bodyString := string(bodyBytes)
-  fmt.Fprintf(w, bodyString)
+  err := r.ParseForm()
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusBadRequest)
+    return
+  }
+
+  // Create a new recipe
+  newRecipe := recipe.Recipe{
+    Name: r.FormValue("r_name"),
+    Yields: r.FormValue("yields"),
+    KeepsFor: r.FormValue("keeps_for"),
+    PrepTime: r.FormValue("prep_time"),
+    TotalTime: r.FormValue("total_time"),
+  }
+
+  // Process ingredients
+  quantities := r.Form["i_quantity[]"]
+  names := r.Form["i_name[]"]
+  preps := r.Form["i_prep[]"]
+  notes := r.Form["i_note[]"]
+
+  for i := 0; i < len(quantities) && i < len(names); i++ {
+    ingredient := recipe.Ingredient{
+      Quantity: getValue(quantities, i),
+      Name:     getValue(names, i),
+      Prep:     getValue(preps, i),
+      Note:     getValue(notes, i),
+    }
+    newRecipe.Ingredients = append(newRecipe.Ingredients, ingredient)
+  }
+
+  // Process instructions
+  steps := r.Form["step[]"]
+  for _, step := range steps {
+    if step != "" {
+      newRecipe.Instructions = append(newRecipe.Instructions, step)
+    }
+  }
+
+  // Process notes
+  recipeNotes := r.Form["note[]"]
+  for _, note := range recipeNotes {
+    if note != "" {
+      newRecipe.Notes = append(newRecipe.Notes, note)
+    }
+  }
+
+  // Process tags
+  tags := r.Form["tag[]"]
+  for _, tag := range tags {
+    if tag != "" {
+      newRecipe.Tags = append(newRecipe.Tags, tag)
+    }
+  }
+
+  // Process source
+  if r.FormValue("s_name") != "" {
+    sourceType := recipe.Type(r.FormValue("s_type"))
+    newRecipe.Source = recipe.Source{
+      Name: r.FormValue("s_name"),
+      Type: sourceType,
+      Link: r.FormValue("s_link"),
+    }
+  }
+
+  // Save to database
+  _, err = db.Save(&newRecipe, uuid.New().String(), "")
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  // Show success page
+  templateBytes := appTemplate.MustAsset("web/template/create_recipe.html")
+  t := template.New("create_recipe.html")
+  t.Parse(string(templateBytes[:]))
+  t.Execute(w, map[string]bool{"Success": true})
+}
+
+func getValue(slice []string, index int) string {
+  if index < len(slice) {
+    return slice[index]
+  }
+  return ""
 }
