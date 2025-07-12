87a98fa72cb471d1e6cdfbae9f9e0ddfbbcb9fd8
Mike Yachanin <michael@yachanin.com>
2025-07-12 02:38:46 -0400

    chore: Add config for local ops tool

--- db/ops/config.go
+++ db/ops/config.go
@@ -1,6 +1,19 @@
 // Defines the config path for db operations
 package ops
 
-const (
-  configPath = "../../config_json"
-)
+import "os"
+
+var configPath = getConfigPath()
+
+func getConfigPath() string {
+  if path := os.Getenv("COOKBOOK_CONFIG"); path != "" {
+    return path
+  }
+  if _, err := os.Stat("config_local.json"); err == nil {
+    return "config_local.json"
+  }
+  if _, err := os.Stat("../../config_json"); err == nil {
+    return "../../config_json"
+  }
+  return "config_json"
+}
